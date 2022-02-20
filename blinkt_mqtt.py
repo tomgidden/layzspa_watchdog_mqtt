7c1d8e2b16bfad63fed242436d0da800bf8de894
Tom Gidden <tom@gidden.net>
2022-02-20 03:35:42 +0000

    Add UDP mode, Blinkt script, startups, improvements

---
+++ blinkt_mqtt.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+
+from sys import exit
+
+try:
+    import paho.mqtt.client as mqtt
+except ImportError:
+    exit("This example requires the paho-mqtt module\nInstall with: sudo pip install paho-mqtt")
+
+import blinkt
+import time
+import threading
+import signal
+
+MQTT_SERVER = "mqtt.home"
+MQTT_PORT = 1883
+MQTT_TOPIC = "blinkt1"
+
+# Set these to use authorisation
+MQTT_USER = None
+MQTT_PASS = None
+
+"""
+rgb,<pixel>,<r>,<g>,<b> - Set a single pixel to an RGB colour. Example: rgb,1,255,0,255
+clr - Clear Blinkt!
+"""
+
+exiting = False
+
+leds = None
+lock = threading.Lock()
+
+def clr_leds ():
+    global leds
+
+    leds = [(0,0,0,None,None,None,0,None) for x in range(blinkt.NUM_PIXELS)]
+    for x in range(blinkt.NUM_PIXELS):
+        leds[x] = (0,0,0,0,0,0,0,0)
+
+clr_leds()
+
+def set_led(x, r1, g1, b1, r2, g2, b2, dur):
+    leds[x] = (r1,g1,b1,r2,g2,b2,dur,time.time()+dur)
+
+class LEDUpdateJob(threading.Thread):
+    def __init__(self, interval=0.1):
+        self.event = threading.Event()
+        self.interval = interval
+        super(LEDUpdateJob,self).__init__()
+
+    def run(self):
+        while not self.event.wait(self.interval):
+            if lock.acquire(False):
+                update = False
+                now = time.time()
+                for x in range(blinkt.NUM_PIXELS):
+                    r1, g1, b1, r2, g2, b2, dur, unt = leds[x]
+                    if dur is not None:
+                        if dur != 0:
+                            if (now > unt):
+                                update = True
+                                blinkt.set_pixel(x, r1, g1, b1)
+                                leds[x] = (r2,g2,b2,r1,g1,b1,dur,now+dur)
+                        else:
+                            update = True
+                            blinkt.set_pixel(x, r1, g1, b1)
+                            leds[x] = (r1,g1,b1,None,None,None,None,None)
+
+                if update is True: blinkt.show()
+                lock.release()
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe(MQTT_TOPIC)
+
+def handle_cmd (cmd, msg):
+    try:
+        data = cmd.split(',')
+    except Exception as e:
+        print (e)
+        return
+
+    command = data.pop(0)
+
+    if command == "clr" and len(data) == 0:
+        clr_leds()
+        blinkt.clear()
+        return True
+
+    if command == "rgb" and len(data) == 4:
+        data = [data[0],
+                data[1], data[2], data[3],
+                data[1], data[2], data[3],
+                0]
+
+    if command == "rgb" and len(data) == 8:
+        try:
+            pixel = data.pop(0)
+            if pixel == "*":
+                pixel = None
+            else:
+                pixel = int(pixel)
+                if pixel > 7:
+                    print("Pixel out of range: " + str(pixel))
+                    return False
+
+            r1 = int(data[0]) & 0xff
+            g1 = int(data[1]) & 0xff
+            b1 = int(data[2]) & 0xff
+            r2 = int(data[3]) & 0xff
+            g2 = int(data[4]) & 0xff
+            b2 = int(data[5]) & 0xff
+            dur = float(data[6])
+
+        except ValueError:
+            print("Malformed command: " + str(msg.payload))
+            return False
+
+        if pixel is None:
+            for x in range(blinkt.NUM_PIXELS):
+                set_led(x, r1, g1, b1, r2, g2, b2, dur)
+                return True
+        else:
+            set_led(pixel, r1, g1, b1, r2, g2, b2, dur)
+            return True
+
+    return False
+
+
+def on_message(client, userdata, msg):
+
+    if lock.acquire(True):
+        cmds = msg.payload.decode("utf-8").split(';')
+        change = False
+
+        for cmd in cmds:
+            if handle_cmd(cmd, msg):
+                change = True
+
+        if change:
+            blinkt.show()
+        lock.release()
+
+    return
+
+
+blinkt.set_clear_on_exit()
+
+l = LEDUpdateJob(0.1)
+l.daemon = True
+l.start()
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+if MQTT_USER is not None and MQTT_PASS is not None:
+    print("Using username: {un} and password: {pw}".format(un=MQTT_USER, pw="*" * len(MQTT_PASS)))
+    client.username_pw_set(username=MQTT_USER, password=MQTT_PASS)
+
+client.connect(MQTT_SERVER, MQTT_PORT, 60)
+
+client.loop_forever()
--- layzspa_watchdog_mqtt.py
+++ layzspa_watchdog_mqtt.py
@@ -1,35 +1,130 @@
+#!/usr/bin/env python
+
+import os
+def getenv(k):
+    try:
+        return os.environ[k]
+    except:
+        return None
+
+mqtt_host = getenv('MQTT_HOST')
+mqtt_port = int(getenv('MQTT_PORT') or 0)
+mqtt_topic = getenv('MQTT_TOPIC')
+loop_seconds = int(getenv('LOOP_SECONDS') or 60)
+email = getenv("LAYZSPA_EMAIL")
+password = getenv("LAYZSPA_PASSWORD")
+ping_host = getenv('PING_HOST')
+ping_delay = int(getenv('PING_DELAY') or 60)
+udp_mac = getenv('UDP_MAC')
+
+from sys import exit
 import json
 import getpass
 from pprint import pprint
 from pathlib import Path
-import os
 import asyncio
-from layz_spa.auth import Auth
-from layz_spa.spa import Spa
-from layz_spa.errors import Error
+import threading
+import datetime
+
+try:
+    from layz_spa.auth import Auth
+    from layz_spa.spa import Spa
+    from layz_spa.errors import Error
+except:
+    print ("pip3 install layz_spa for API support")
+    email = None
+    password = None
+
+import time
 
 import paho.mqtt.client as paho
+import ping3
+import socket
+import binascii
+import signal
 
-mqtt_host = os.environ['MQTT_HOST']
-mqtt_port = int(os.environ['MQTT_PORT'])
-mqtt_topic = os.environ['MQTT_TOPIC']
-loop_seconds = int(os.environ['LOOP_SECONDS'])
-email = os.environ["LAYZSPA_EMAIL"]
-password = os.environ["LAYZSPA_PASSWORD"]
 
 mqtt = paho.Client("layzspa_watchdog")
 mqtt.connect(mqtt_host, mqtt_port, 60)
 
-def mqtt_send(payload):
+running = True
+debug = True
+
+def ping_main():
+    if not ping_host:
+        print ("set PING_HOST for ping monitoring")
+        return
+
+    while running:
+        ping = ping3.ping(ping_host, timeout=10)
+        mqtt_send('ping', ping)
+        time.sleep(ping_delay)
+
+def udp_main():
+    if not udp_mac:
+        print ("set UDP_MAC for local network monitoring")
+        return
+
     try:
-        mqtt.publish(mqtt_topic, payload)
+        UDP_IP = "255.255.255.255"
+        UDP_PORT = 12414
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((UDP_IP, UDP_PORT))
+        macbytes = binascii.unhexlify(str(udp_mac).replace(':', ''))
+    except Exception as e:
+        print ("Failed to set up local network monitoring", e)
+        return
+
+    while running:
+        data, addr = sock.recvfrom(1024)
+        if data == b'\x00\x00\x00\x03\x03\x00\x00\x03':
+            pass
+
+        else:
+            if data[9:15] == macbytes: #b'@\xf5 \xf4\x11\xef':
+                status = data[16]
+                temp = int(data[17])
+                curtemp = int(data[30])
+                ok = data[31] != 0
+                on   = status & 0b1 != 0
+                heat = status & 0b10 != 0
+                filt = status & 0b100 != 0
+                mass = status & 0b1000 != 0
+                lock = status & 0b10000 != 0
+                cels = status & 0b1000000 != 0
+
+                d = {
+                    'updated_at': str(datetime.datetime.now()),
+                    'power': status & 0b1 != 0,
+                    'heat_power': status & 0b10 != 0,
+                    'filter_power': status & 0b100 != 0,
+                    'wave_power': status & 0b1000 != 0,
+                    'locked': status & 0b10000 != 0,
+                    'temp_set_unit': ("°C" if status & 0b1000000 != 0 else "°F"),
+                    'temp_now': curtemp,
+                    'temp_set': temp,
+                    'heat_temp_reach': curtemp == temp,
+                    'ok': ok,
+                    'online': True
+                };
+
+                mqtt_send('udp', json.dumps(d))
+            else:
+                print ("Unknown MAC address {} != {}".format(data[9:15], macbytes))
+
+
+def mqtt_send(topic, payload):
+    try:
+        if debug: print("{}\t{}".format(topic,payload))
+
+        mqtt.publish(mqtt_topic+'/'+topic, payload)
     except Exception as e:
         print (e)
         mqtt.connect(mqtt_host, mqtt_port, 60)
-        mqtt.publish(mqtt_topic, payload)
+        mqtt.publish(mqtt_topic+'/'+topic, payload)
 
 
-async def auth():
+async def api_auth():
     cache_file = Path("token.cache")
 
     try:
@@ -53,22 +148,50 @@ async def auth():
 
         return (spa,online)
 
-async def update():
-    (spa,online) = await auth()
+async def api_update():
+    (spa,online) = await api_auth()
 
     await spa.update_status()
 
     d = {k: v for k, v in spa.__dict__.items() if k != 'api'}
     d['updated_at'] = str(d['updated_at'])
     d['online'] = online
-    mqtt.publish(mqtt_topic, json.dumps(d))
 
-async def main():
+    mqtt_send('api', json.dumps(d))
+
+async def api_loop():
+    if not email: return
+
     loop = asyncio.get_running_loop()
-    while True:
-        mqtt.loop(.5)
-        await update()
+    while running:
+        await api_update()
         await asyncio.sleep(loop_seconds)
 
+def api_main():
+    asyncio.run(api_loop())
+
+def shutdown_handler(signum, frame):
+    global U, A, P
+    running = False
+    U.stop()
+    A.stop()
+    P.stop()
+    mqtt.disconnect()
+signal.signal(signal.SIGINT, shutdown_handler)
+signal.signal(signal.SIGTERM, shutdown_handler)
+signal.signal(signal.SIGUSR1, shutdown_handler)
+
 if __name__ == "__main__":
-    asyncio.run(main())
+    U = threading.Thread(target=udp_main)
+    U.daemon = True
+    U.start()
+
+    A = threading.Thread(target=api_main)
+    A.daemon = True
+    A.start()
+
+    P = threading.Thread(target=ping_main)
+    P.daemon = True
+    P.start()
+
+    mqtt.loop_forever()
