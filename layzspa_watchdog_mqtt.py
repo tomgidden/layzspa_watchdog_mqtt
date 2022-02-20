af42671e97f2d576401a20e49bbf8d5c056cb518
Tom Gidden <tom@gidden.net>
2022-02-20 23:59:15 +0000

    Fix bad JSON out of ping

--- layzspa_watchdog_mqtt.py
+++ layzspa_watchdog_mqtt.py
@@ -57,7 +57,7 @@ def ping_main():
 
     while running:
         ping = ping3.ping(ping_host, timeout=10)
-        mqtt_send('ping', ping)
+        mqtt_send('ping', json.dumps(ping))
         time.sleep(ping_delay)
 
 def udp_main():
